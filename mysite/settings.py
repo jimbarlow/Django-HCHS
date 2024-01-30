37b36f36f8f50e3e4ec2de926eae626ad312498e
Jim Barlow <jim@jimbarlow.com>
2024-01-30 14:12:29 -0500

    added allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://django-server-production', 'volunteers.harthumane.org', 'localhost']
 
 # # The following added by Jim for allauth
 SITE_ID = 1
@@ -60,7 +60,7 @@ SOCIALACCOUNT_PROVIDERS = {
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 
-CSRF_TRUSTED_ORIGINS = ['localhost:8000', 'https://volunteers.harthumane.org', 'https://django-server-production-2070.up.railway.app' ]
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'https://volunteers.harthumane.org', 'https://django-server-production-2070.up.railway.app' ]
 
 # Application definition
 
